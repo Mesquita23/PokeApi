00f0a60b17979c2e3ed606fbbd357d792244e766
Gabriel Mesquita <gabriel.mesquita@basis.com.br>
2021-07-13 16:56:05 -0300

    iniciando função batalha

--- pokeAPI.js
+++ pokeAPI.js
@@ -2,11 +2,63 @@ const readline = require('readline-sync')
 const axios = require('axios')
 
 async function start(){
+    const squirtleJson = await returnJSON("squirtle")
+    var squirtle = createObjectPokemon(squirtleJson.data)
     
     const pokemonTyped = searchPokemon()
     const api = await returnJSON(pokemonTyped)
-    var pokemon = createObjectPokemon(api.data)
     
+    var pokemon = createObjectPokemon(api.data)
+
+    battle(squirtle, pokemon)
+
+    
+    
+}
+
+function showWinner(pokemon1, pokemon2){
+    if (pokemon1.hp <= 0 ){
+        console.log(`-------------------- ${pokemon2.name} venceu ---------------------------`)
+    }else if(pokemon2.hp <= 0 ){
+        console.log(`-------------------- ${pokemon1.name} venceu ---------------------------`)
+    }
+}
+
+function battle(pokemon1, pokemon2){
+    while (pokemon1.hp > 0 && pokemon2.hp > 0){
+        attack(pokemon1, pokemon2)
+        attack(pokemon2, pokemon1)
+
+        showWinner(pokemon1, pokemon2)
+    }
+}
+
+function attack(pokemon1, pokemon2){
+    const defense = defenseOf(pokemon2)
+    console.log(defense)
+    const damage = pokemon1.attack * defense
+    console.log(`attack do ${pokemon1.name}: ${damage}`)
+    pokemon2.hp = pokemon2.hp - damage
+    console.log(`${pokemon2.name}: ${pokemon2.hp} hp`)
+}
+
+function defenseOf(pokemon){
+    const def = (pokemon.defense/100)
+    if(def > 0.5 ){
+        return 0.5
+    }else{
+        return def
+    }
+}
+
+function returnTruebyChance(chance){
+    const random = Math.random();
+    percantage = (parseInt(chance)/100)
+    if(random < percantage){
+        return true
+    }
+    return false
+
 }
 
 function searchPokemon(){
@@ -25,6 +77,7 @@ function createObjectPokemon(data){
     object.weight = `${parseFloat(data.weight/10)} kg`
     object.height = `${parseFloat(data.height/10)} m`
     getStatus(object ,data.stats)
+    console.log(object)
     return object
 
 }
